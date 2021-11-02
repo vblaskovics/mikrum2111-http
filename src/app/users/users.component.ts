9feaf334d74727f9c76880875215093fb5692d16
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-11-02 11:48:07 +0100

    Add timer to users component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users',
@@ -14,9 +16,15 @@ export class UsersComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(res => {
+    // this.userService.getUsers().subscribe(res => {
+    //   this.users = res;
+    // })
+
+    timer(0, 1000).pipe(
+      switchMap(() => this.userService.getUsers())
+    ).subscribe(res => {
       this.users = res;
-    })
+    });
   }
 
 }
