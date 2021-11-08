7d91ec0db07b9d6762bafdbdaf7bc0a7f4051cac
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-11-08 14:22:17 +0100

    Add async/await example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { timeout } from 'rxjs/operators';
+import { PostService } from './post/post.service';
 import { UserService } from './user/user.service';
 
 @Component({
@@ -9,12 +11,24 @@ import { UserService } from './user/user.service';
 export class AppComponent {
   title = 'mikrum2111-http';
 
-  constructor(private userService:UserService){}
+  constructor(private userService:UserService, private postService:PostService){}
+
+  createPosts = async () => {
+    let users = await this.userService.getUsers().toPromise();
+    let posts = await this.postService.getPosts().pipe(
+      timeout(30000)
+    ).toPromise();
+
+    let results:any[] = [];
+    posts.forEach((p) => {
+      let username = users.find(u => u.id === p.userId)?.username;
+      results.push({...p, username});
+    })
+    console.log(results);
+  }
 
   ngOnInit(){
     console.log('RXJS + HttpClient');
-    
-
-
+    this.createPosts();
   }
 }
